df36341d44e0e7063a99b6e8c341634b44bf3330
bf14 <bf14@sanger.ac.uk>
2025-01-13 11:59:17 +0000

    Fix path to mock files directory

--- tests/mocks/mock_files.py
+++ tests/mocks/mock_files.py
@@ -6,7 +6,7 @@ def get_example_sample_metadata_xlsx() -> Path:
     """
     Return the path to a xlsx file containing example sample metadata
     """
-    file = resources.files("test.mocks").joinpath("example_sample_metadata.xlsx")
+    file = resources.files("tests.mocks").joinpath("example_sample_metadata.xlsx")
     as_path = Path(str(file))
     if not as_path.exists():
         raise FileNotFoundError(f"File not found: {as_path}")
